d132b39ae95d8ebda1b88e6fe572bc19e5ce2807
keitkalon <keitkalon@gmail.com>
2021-05-03 16:12:34 +0300

    the orcs & the trolls chase the player all around

--- game_map.py
+++ game_map.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from typing import Iterable, Optional, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 import numpy as np 
 from tcod.console import Console
+from entity import Actor
 import tile_types
 
 if TYPE_CHECKING:
@@ -20,6 +21,15 @@ class GameMap:
         self.visible = np.full((width, height), fill_value=False, order="F")  # Tiles the player can currently see
         self.explored = np.full((width, height), fill_value=False, order="F")  # Tiles the player has seen before
 
+    @property
+    def actors(self) -> Iterator[Actor]:
+        """Iterate over this maps living actors."""
+        yield from (
+            entity
+            for entity in self.entities
+            if isinstance(entity, Actor) and entity.is_alive
+        )
+
     def get_blocking_entity_at_location(self, location_x: int, location_y: int,) -> Optional[Entity]:
         for entity in self.entities:
             if (
@@ -28,6 +38,13 @@ class GameMap:
                 and entity.y == location_y
             ):
                 return entity
+
+        return None
+
+    def get_actor_at_location(self, x: int, y: int) -> Optional[Actor]:
+        for actor in self.actors:
+            if actor.x == x and actor.y == y:
+                return actor
 
         return None
 
