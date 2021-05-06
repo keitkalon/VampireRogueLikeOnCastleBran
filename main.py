30448b5dccfb925b84b002a6bfbb1e10f36337ac
keitkalon <keitkalon@gmail.com>
2021-05-06 18:38:48 +0300

    add health potions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+import traceback
 import tcod
 
 import color
@@ -18,6 +19,7 @@ def main():
     max_rooms = 30
 
     max_monsters_per_room = 2
+    max_items_per_room = 2
     
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -35,6 +37,7 @@ def main():
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
+        max_items_per_room=max_items_per_room,
         engine=engine,
     )
 
@@ -56,13 +59,17 @@ def main():
         while True:  
             root_console.clear()
             engine.event_handler.on_render(console=root_console)
-            context.present(root_console)          
-            
+            context.present(root_console)
 
-            engine.event_handler.handle_events(context)
-
-            
-
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception:  # Handle exceptions in game.
+                traceback.print_exc()  # Print error to stderr.
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)         
+ 
 
 if __name__ == "__main__":
     main()
--- procgen.py
+++ procgen.py
@@ -37,8 +37,9 @@ class RectangularRoom:
             and self.y2 >= other.y1
         )
 
-def place_entities(room: RectangularRoom, dungeon: GameMap, maximum_monsters: int) -> None:
+def place_entities(room: RectangularRoom, dungeon: GameMap, maximum_monsters: int, maximum_items: int) -> None:
     number_of_monsters = random.randint(0, maximum_monsters)
+    number_of_items = random.randint(0, maximum_items)
 
     for i in range(number_of_monsters):
         x = random.randint(room.x1 + 1, room.x2 - 1)
@@ -49,6 +50,13 @@ def place_entities(room: RectangularRoom, dungeon: GameMap, maximum_monsters: in
                 entity_factories.orc.spawn(dungeon, x, y)
             else:
                 entity_factories.troll.spawn(dungeon, x, y)
+
+    for i in range(number_of_items):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
+
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            entity_factories.health_potion.spawn(dungeon, x, y)
 
 
 def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
@@ -75,6 +83,7 @@ def generate_dungeon(
    map_width: int,
    map_height: int,
    max_monsters_per_room: int,
+   max_items_per_room: int,
    engine: Engine,
 ) -> GameMap:
     """Generate a new dungeon map."""
@@ -108,7 +117,7 @@ def generate_dungeon(
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
-        place_entities(new_room, dungeon, max_monsters_per_room)
+        place_entities(new_room, dungeon, max_monsters_per_room, max_items_per_room)
         # Finally, append the new room to the list.
         rooms.append(new_room)
     return dungeon
